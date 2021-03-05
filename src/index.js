b30adcfa252c904c6250f8c987b99949bf0c5363
bigwizzo <wnnwillard@gmail.com>
2021-03-05 19:38:29 +0200

    Add weather description

--- src/index.js
+++ src/index.js
@@ -6,17 +6,40 @@ function getWeather(city){
       return response.json();
     })
     .then(function(response) {
-      console.log(response.main.temp)
-      document.querySelector("#current-temp").textContent = response.main.temperature;
-      console.log(response.main)
+      console.log(response.weather[0].icon)
+      const temp = document.querySelector("#current-temp");
+      temp.innerHTML = `
+      <div class="card">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <span class="font-weight-bolder">${response.name}</span>
+          </li>
+          <li class="list-group-item">
+            <span class="font-weight-bolder">Temperature: </span><span>${response.main.temp}</span>
+          </li>
+          <li class="list-group-item">
+            <span class="font-weight-bolder"><img src="http://openweathermap.org/img/wn/${response.weather[0].icon}.png" alt=""></span><span>${response.weather[0].description}</span>
+          </li>
+        </ul>
+      </div>
+      `;
     });
+}
+
+function showAlert(message, className) {
+  const div = document.createElement('div');
+  div.className = `alert alert-${className}`;
+  div.appendChild(document.createTextNode(message));
+  const alertDiv = document.querySelector('.alert-div');
+  alertDiv.appendChild(div);
+  setTimeout(() => document.querySelector('.alert').remove(), 3000);
 }
 
 document.querySelector('#city-form').addEventListener('submit', (e) => {
   e.preventDefault();
   const cityName = document.querySelector('#city-name').value;
   if (cityName === '') {
-    return alert('Please fill in all the fields', 'danger');
+    showAlert('Please fill in all the fields', 'danger');
   } else {
       getWeather(cityName);
   }
