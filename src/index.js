d1d8fcf7d2a8a30a0eb7de16ac54d4e066681b0d
bigwizzo <wnnwillard@gmail.com>
2021-03-07 23:37:03 +0200

    Add icon, fahrenheit, sunrise and sunset variables

--- src/index.js
+++ src/index.js
@@ -22,6 +22,10 @@ window.addEventListener('load', () => {
          const place = data.name;
          const { description, icon } = data.weather[0];
          const { sunrise, sunset } = data.sys;
+         const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+         const fahrenheit = (temp * 9) / 5 + 32;
+         const sunriseGMT = new Date(sunrise * 1000);
+         const sunsetGMT = new Date(sunset * 1000);
        });
     });
   }
@@ -36,59 +40,58 @@ function showAlert(message, className) {
   setTimeout(() => document.querySelector('.alert').remove(), 3000);
 }
 
-// function getWeather(city, item) {
-  // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e5b18a07839188e5de8e8db9b8a49386&units=${item}`, { mode: 'cors' })
-    // .then((response) => response.json())
-    // .then((response) => {
-      // const temp = document.querySelector('#current-temp');
-      // temp.innerHTML = `
-      // <div class="card">
-        // <ul class="list-group list-group-flush">
-          // <li class="list-group-item">
-            // <span class="font-weight-bolder">${response.main.temp}°</span>
-          // </li>
-          // <li class="list-group-item">
-          // <span>${response.weather[0].description}</span><span class="font-weight-bolder"><img src="http://openweathermap.org/img/wn/${response.weather[0].icon}.png" alt=""></span>
-          // </li>
-        // </ul>
-      // </div>
-      // `;
-    // }).catch(() => {
-      // showAlert('Enter a valid city', 'danger');
-    // });
-// }
-// 
-// const unitDiv = () => {
-  // const unitWrap = document.querySelector('.unit-wrap');
-  // unitWrap.innerHTML = `
-    // <div class="form-check form-switch">
-      // <input
-        // class="form-check-input"
-        // type="checkbox"
-        // id="flexCheckChecked"
-        // checked
-      // />
-      // <label class="form-check-label" for="flexCheckChecked">Celcius</label>
-    // </div>
-  // `;
-// }
-// 
-// document.querySelector('#city-form').addEventListener('submit', (e) => {
-  // e.preventDefault();
-  // const cityName = document.querySelector('#city-name').value;
-  // if (cityName === '') {
-    // showAlert('Please fill in all the fields', 'danger');
-  // } else {
-    // unitDiv();
-    // const unit = document.querySelector('#flexCheckChecked');
-    // console.log(unit.checked)
-      // if (unit.checked) {
-        // console.log('metric')
-        // getWeather(cityName, 'metric');;
-      // } else {
-        // console.log('imperial')
-        // getWeather(cityName, 'imperial');;
-      // }    
-  // }
-// });
-// 
+function getWeather(city, item) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e5b18a07839188e5de8e8db9b8a49386&units=${item}`, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((response) => {
+      const temp = document.querySelector('#current-temp');
+      temp.innerHTML = `
+      <div class="card">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <span class="font-weight-bolder">${response.main.temp}°</span>
+          </li>
+          <li class="list-group-item">
+          <span>${response.weather[0].description}</span><span class="font-weight-bolder"><img src="http://openweathermap.org/img/wn/${response.weather[0].icon}.png" alt=""></span>
+          </li>
+        </ul>
+      </div>
+      `;
+    }).catch(() => {
+      showAlert('Enter a valid city', 'danger');
+    });
+}
+
+const unitDiv = () => {
+  const unitWrap = document.querySelector('.unit-wrap');
+  unitWrap.innerHTML = `
+    <div class="form-check form-switch">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        id="flexCheckChecked"
+        checked
+      />
+      <label class="form-check-label" for="flexCheckChecked">Celcius</label>
+    </div>
+  `;
+}
+
+document.querySelector('#city-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const cityName = document.querySelector('#city-name').value;
+  if (cityName === '') {
+    showAlert('Please fill in all the fields', 'danger');
+  } else {
+    unitDiv();
+    const unit = document.querySelector('#flexCheckChecked');
+    console.log(unit.checked)
+      if (unit.checked) {
+        console.log('metric')
+        getWeather(cityName, 'metric');;
+      } else {
+        console.log('imperial')
+        getWeather(cityName, 'imperial');;
+      }    
+  }
+});
