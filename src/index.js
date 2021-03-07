2a712df01d45a76770a5ca4c86a7093523a7a75f
bigwizzo <wnnwillard@gmail.com>
2021-03-07 14:37:40 +0200

    Add object destructuring

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import './styles.css';
 
+const api = 'e5b18a07839188e5de8e8db9b8a49386';
+
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  // Accessing Geolocation of User
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+       console.log(position)
+       long = position.coords.longitude;
+       lat = position.coords.latitude;
+       const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+       fetch(base)
+       .then((response) => {
+         return response.json();
+       })
+       .then((data) => {
+         console.log(data.sys.sunrise);
+         const { temp } = data.main;
+         const place = data.name;
+         const { description, icon } = data.weather[0];
+         const { sunrise, sunset } = data.sys;
+       });
+    });
+  }
+});
+
 function showAlert(message, className) {
   const div = document.createElement('div');
   div.className = `alert alert-${className}`;
@@ -9,58 +36,59 @@ function showAlert(message, className) {
   setTimeout(() => document.querySelector('.alert').remove(), 3000);
 }
 
-function getWeather(city, item) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e5b18a07839188e5de8e8db9b8a49386&units=${item}`, { mode: 'cors' })
-    .then((response) => response.json())
-    .then((response) => {
-      const temp = document.querySelector('#current-temp');
-      temp.innerHTML = `
-      <div class="card">
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">
-            <span class="font-weight-bolder">${response.main.temp}°</span>
-          </li>
-          <li class="list-group-item">
-          <span>${response.weather[0].description}</span><span class="font-weight-bolder"><img src="http://openweathermap.org/img/wn/${response.weather[0].icon}.png" alt=""></span>
-          </li>
-        </ul>
-      </div>
-      `;
-    }).catch(() => {
-      showAlert('Enter a valid city', 'danger');
-    });
-}
-
-const unitDiv = () => {
-  const unitWrap = document.querySelector('.unit-wrap');
-  unitWrap.innerHTML = `
-    <div class="form-check form-switch">
-      <input
-        class="form-check-input"
-        type="checkbox"
-        id="flexCheckChecked"
-        checked
-      />
-      <label class="form-check-label" for="flexCheckChecked">Celcius</label>
-    </div>
-  `;
-}
-
-document.querySelector('#city-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const cityName = document.querySelector('#city-name').value;
-  if (cityName === '') {
-    showAlert('Please fill in all the fields', 'danger');
-  } else {
-    unitDiv();
-    const unit = document.querySelector('#flexCheckChecked');
-    console.log(unit.checked)
-      if (unit.checked) {
-        console.log('metric')
-        getWeather(cityName, 'metric');;
-      } else {
-        console.log('imperial')
-        getWeather(cityName, 'imperial');;
-      }    
-  }
-});
+// function getWeather(city, item) {
+  // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e5b18a07839188e5de8e8db9b8a49386&units=${item}`, { mode: 'cors' })
+    // .then((response) => response.json())
+    // .then((response) => {
+      // const temp = document.querySelector('#current-temp');
+      // temp.innerHTML = `
+      // <div class="card">
+        // <ul class="list-group list-group-flush">
+          // <li class="list-group-item">
+            // <span class="font-weight-bolder">${response.main.temp}°</span>
+          // </li>
+          // <li class="list-group-item">
+          // <span>${response.weather[0].description}</span><span class="font-weight-bolder"><img src="http://openweathermap.org/img/wn/${response.weather[0].icon}.png" alt=""></span>
+          // </li>
+        // </ul>
+      // </div>
+      // `;
+    // }).catch(() => {
+      // showAlert('Enter a valid city', 'danger');
+    // });
+// }
+// 
+// const unitDiv = () => {
+  // const unitWrap = document.querySelector('.unit-wrap');
+  // unitWrap.innerHTML = `
+    // <div class="form-check form-switch">
+      // <input
+        // class="form-check-input"
+        // type="checkbox"
+        // id="flexCheckChecked"
+        // checked
+      // />
+      // <label class="form-check-label" for="flexCheckChecked">Celcius</label>
+    // </div>
+  // `;
+// }
+// 
+// document.querySelector('#city-form').addEventListener('submit', (e) => {
+  // e.preventDefault();
+  // const cityName = document.querySelector('#city-name').value;
+  // if (cityName === '') {
+    // showAlert('Please fill in all the fields', 'danger');
+  // } else {
+    // unitDiv();
+    // const unit = document.querySelector('#flexCheckChecked');
+    // console.log(unit.checked)
+      // if (unit.checked) {
+        // console.log('metric')
+        // getWeather(cityName, 'metric');;
+      // } else {
+        // console.log('imperial')
+        // getWeather(cityName, 'imperial');;
+      // }    
+  // }
+// });
+// 
