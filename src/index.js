4b4fb1090c889b0990c2f3e848c12d866ed05065
bigwizzo <wnnwillard@gmail.com>
2021-03-08 02:22:30 +0200

    Add error handling to catch invalid city

--- src/index.js
+++ src/index.js
@@ -1,45 +1,47 @@
 import './styles.css';
 
 const api = 'e5b18a07839188e5de8e8db9b8a49386';
-
 const iconImg = document.getElementById('icon');
 const location = document.querySelector('#location');
 const tempC = document.querySelector('.c');
 const desc = document.querySelector('.description')
-const sunriseDOM = document.querySelector('.sunrise');
-const sunsetDOM = document.querySelector('.sunset');
 const unit = document.querySelector('#flexCheckChecked');
 
-       const base = `https://api.openweathermap.org/data/2.5/weather?q=kinshasa&appid=${api}&units=metric`;
-       fetch(base)
-       .then((response) => {
-         return response.json();
-       })
-       .then((data) => {
-         console.log(data.sys.sunrise);
-         const { temp } = data.main;
-         const place = data.name;
-         const { description, icon } = data.weather[0];
-         const { sunrise, sunset } = data.sys;
-         const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-         const fahrenheit = (temp * 9) / 5 + 32;
-         const sunriseGMT = new Date(sunrise * 1000);
-         const sunsetGMT = new Date(sunset * 1000);
+function getWeather(city) {
+  const base = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`;
+  fetch(base)
+  .then((response) => response.json())
+  .then((data) => {
+    const { temp } = data.main;
+    const place = data.name;
+    const { description, icon } = data.weather[0];
+    const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    const fahrenheit = (temp * 9) / 5 + 32;
+    iconImg.src = iconUrl;
+    location.textContent = `${place}`;
+    desc.textContent = `${description}`;
+    tempC.textContent = `${temp.toFixed(2)} °C`;
+    unit.addEventListener("click", function(){
+      if (tempC.textContent == `${temp.toFixed(2)} °C`) {
+       tempC.textContent = `${fahrenheit.toFixed(2)} °F`;
+      } else if (tempC.textContent == `${fahrenheit.toFixed(2)} °F`) {
+       tempC.textContent = `${temp.toFixed(2)} °C`;
+      }
+     });
+  }).catch(() => {
+    showAlert('Please enter a valid city', 'danger');
+  });
+}
 
-         iconImg.src = iconUrl;
-         location.textContent = `${place}`;
-         desc.textContent = `${description}`;
-         tempC.textContent = `${temp.toFixed(2)} °C`;
-         unit.addEventListener("click", function(){
-           if (tempC.textContent == `${temp.toFixed(2)} °C`) {
-            tempC.textContent = `${fahrenheit.toFixed(2)} °F`;
-           } else if (tempC.textContent == `${fahrenheit.toFixed(2)} °F`) {
-            tempC.textContent = `${temp.toFixed(2)} °C`;
-           }
-          });
-         sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
-         sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
-       });
+document.querySelector('#city-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const cityName = document.querySelector('#city-name').value;
+  if (cityName === '') {
+    showAlert('Please fill in all the fields', 'danger');
+  } else {
+    getWeather(cityName);
+  }
+});
 
 
 function showAlert(message, className) {
@@ -77,14 +79,14 @@ function showAlert(message, className) {
   // const unitWrap = document.querySelector('.unit-wrap');
   // unitWrap.innerHTML = `
     // <div class="form-check form-switch">
-      {/* <input */}
+      /* <input */
         // class="form-check-input"
         // type="checkbox"
         // id="flexCheckChecked"
         // checked
       // />
-      {/* <label class="form-check-label" for="flexCheckChecked">Celcius</label> */}
-    {/* </div> */}
+      /* <label class="form-check-label" for="flexCheckChecked">Celcius</label> */
+    /* </div> */
   // `;
 // }
 // 
