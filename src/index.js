3a6399194a319cec6f1ca7558bdb07777369964f
bigwizzo <wnnwillard@gmail.com>
2021-03-08 12:52:24 +0200

    Restructure the UI

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import './styles.css';
 
-const weatherWrap = () => {
-  const weather = document.querySelector('.weather');
-  weather.innerHTML = `
-    <div class="laction">
-      <div class="location" id="location"></div>
-      <div class="description" id="description"></div>
-    </div>
-    <div class="form-check form-switch">
-      <input class="form-check-input" type="checkbox" id="flexCheckChecked" checked/>
-      <label class="form-check-label c" for="flexCheckChecked"></label>
-    </div>
-  `;
-}
-
 function getWeather(city) {
   const api = 'e5b18a07839188e5de8e8db9b8a49386';
   const iconImg = document.getElementById('icon');
   const location = document.querySelector('#location');
-  const tempC = document.querySelector('.c');
   const desc = document.querySelector('.description')
-  const unit = document.querySelector('#flexCheckChecked');
   const base = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`;
   fetch(base)
   .then((response) => response.json())
@@ -33,12 +17,19 @@ function getWeather(city) {
     iconImg.src = iconUrl;
     location.textContent = `${place}`;
     desc.textContent = `${description}`;
-    tempC.textContent = `${temp.toFixed(2)} °C`;
+    const formSwitch = document.querySelector('.form-switch')
+    formSwitch.innerHTML = `
+      <input class="form-check-input float-none" type="checkbox" id="flexCheckChecked" checked/>
+      <label class="form-check-label c" for="flexCheckChecked"></label>
+    `;
+    const tempC = document.querySelector('.c');
+    tempC.textContent = `${temp.toFixed(2)} °Celcius`;
+    const unit = document.querySelector('#flexCheckChecked');
     unit.addEventListener("click", function(){
-      if (tempC.textContent == `${temp.toFixed(2)} °C`) {
-       tempC.textContent = `${fahrenheit.toFixed(2)} °F`;
-      } else if (tempC.textContent == `${fahrenheit.toFixed(2)} °F`) {
-       tempC.textContent = `${temp.toFixed(2)} °C`;
+      if (tempC.textContent == `${temp.toFixed(2)} °Celcius`) {
+       tempC.textContent = `${fahrenheit.toFixed(2)} °Fahrenheit`;
+      } else if (tempC.textContent == `${fahrenheit.toFixed(2)} °Fahrenheit`) {
+       tempC.textContent = `${temp.toFixed(2)} °Celcius`;
       }
      });
   }).catch(() => {
@@ -52,7 +43,6 @@ document.querySelector('#city-form').addEventListener('submit', (e) => {
   if (cityName === '') {
     showAlert('Please fill in all the fields', 'danger');
   } else {
-    weatherWrap();
     getWeather(cityName);
   }
 });
